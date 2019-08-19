77a8b2e9ef1b5f90a184b316057ea9b2cae7c663
Maina Wycliffe <wmmaina7@gmail.com>
2019-08-19 21:07:17 +0300

    add simple hello world component

---
+++ angular-elements/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  // selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+  @Input()
+  name: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
