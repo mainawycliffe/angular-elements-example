adb73fe14d2496e68d493e30cbf98790aaecee50
Maina Wycliffe <wmmaina7@gmail.com>
2019-08-19 21:10:31 +0300

    define custome custom element and bind to hello world

--- angular-elements/src/app/app.module.ts
+++ angular-elements/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { createCustomElement } from '@angular/elements';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
@@ -11,4 +12,15 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
   entryComponents: [HelloWorldComponent]
   // bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const elements: any[] = [[HelloWorldComponent, 'hello-world']];
+
+    for (const [component, name] of elements) {
+      const el = createCustomElement(component, { injector: this.injector });
+      customElements.define(name, el);
+    }
+  }
+}
