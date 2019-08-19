f549284a211c696f2cc077f99d044d4df35e1c84
Maina Wycliffe <wmmaina7@gmail.com>
2019-08-19 21:08:49 +0300

    add hello world component

--- angular-elements/src/app/app.module.ts
+++ angular-elements/src/app/app.module.ts
@@ -2,14 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HelloWorldComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
